47057fd629485955cebef2a0aaa48ad299a4f96d
Ellaiza Jean Balatero <ejbalatero05@gmail.com>
2025-11-17 12:14:18 +0800

    Added Division

--- math.js
+++ math.js
@@ -7,11 +7,17 @@ function diff(num1, num2){
     console.log(diff);
 }
 function prod(num1, num2){
-    prod = num1 + num2;
+    prod = num1 * num2;
     console.log(prod);
+}
+function quot(num1, num2){
+    quot = num1 / num2;
+    console.log(quot);
 }
 sum(2,3);
 diff(5,3);
 prod(11, 5);
+quot(6,3);
+
 
 
