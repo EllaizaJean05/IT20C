b1dd400c96ced5f5cd9b17c482d5bf4e4742dc5c
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-11-20 08:35:06 +0800

    Added while loop

---
+++ while-loop.js
@@ -0,0 +1,33 @@
+// console.log("Descending");
+// i = 5;
+
+// while (i <= 1){
+//     console.log (i);
+//     i--;
+
+// }
+// console.log("Ascending");
+// x = 1;
+
+// while (x <=5){
+//     console.log (x);
+//     x++;
+
+// }
+// output = " ";
+// y=1;
+// while (y<=5){
+//     output = " ";
+//  y++;
+
+// }
+
+arr=[1,2,3];
+output + " ";
+index =0;
+
+while (index <= arr.length -1){
+    output += arr[index] + " ";
+    index++;
+}
+console.log(output);
