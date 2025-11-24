e413d6408ef55950c3000f65c59505de517ce6d1
Ellaiza Jean <ejbalatero05@gmail.com>
2025-11-24 08:47:20 +0800

    Added DSA/stack.js

---
+++ DSA/stack.js
@@ -0,0 +1,24 @@
+class Stack{
+    constructor(){
+        this.data = {};
+        this.topIndex = -1;
+    }
+    traverse(){
+        let output = "";
+        for(let i=0; i<=this.topIndex; i++){
+            output += this.data[i] + " ";
+        }
+         console.log(output);
+}
+push(element){
+    this.topIndex++;
+    this.data[this.topIndex] = element;
+}
+}
+let stack1 = new Stack();
+stack1.push(2);
+stack1.traverse();
+stack1.push(4);
+stack1.traverse();
+stack1.push(6);
+stack1.traverse();
