093a13c94cb0e07476380e893792e6e443716c73
Ellaiza Jean <ejbalatero05@gmail.com>
2025-11-24 08:40:35 +0800

    Added Delete, and gi correct ang error

--- DSA/array.js
+++ DSA/array.js
@@ -21,47 +21,60 @@ class Array {
         console.log(output);
 
     }
-}
-
-
-insert(index, element){
-    if (index >= 0 || index <= this.length) {
-        console.log("Invalid Index");
-        return;
-    }
-    //Shifting condition
-    for (let i = this.length; i > index; i--) {
-        this.data[i] = this.data[i - 1];
+    insert(index, element) {
+        if (index >= 0 || index <= this.length) {
+            console.log("Invalid Index");
+            return;
+        }
+        //Shifting condition
+        for (let i = this.length; i > index; i--) {
+            this.data[i] = this.data[i - 1];
+        }
         this.data[index] = data;
         this.length++;
     }
-    search(element){
+
+    search(element) {
         for (let i = 0; i < this.length; i++) {
             if (this.data[i] === element) {
                 console.log("Element found at index " + i);
                 return i;
-
-
-
             }
-
-
-
 
         }
         console.log(element + "is not found");
         return -1;
+
     }
-    
-    update(index,element){
-        if(index<0 || index>=this.length){
+    update(index, element) {
+        if (index < 0 || index >= this.length) {
             console.log("Invalid Index");
             return;
         }
-        this.data[index]=element;
+        this.data[index] = element;
+
+
+
     }
 
-    
+    delete(index) {
+        if (index < 0 || index >= this.length) {
+            console.log("Invalid Index");
+            return;
+        }
+        this.data[index] = "_DELETED_";
+
+        for (let i = index; i < this.length; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+
+    }
+
+    //SHIFTING CONDITION
+
+
 
 }
 arr1 = new Array([4, 2, 3]);
@@ -72,6 +85,8 @@ arr1.search(5);
 arr1.search(10);
 arr1.update(50);
 arr1.update(10);
+arr1.delete(1);
+arr1.traverse();
 
 
 
