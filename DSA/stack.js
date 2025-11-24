012edc7c935c75e8d8ddd0f0fa046bc4fe3d2d92
Ellaiza Jean <ejbalatero05@gmail.com>
2025-11-24 08:59:02 +0800

    Added push, peek, and pop

--- DSA/stack.js
+++ DSA/stack.js
@@ -1,24 +1,46 @@
-class Stack{
-    constructor(){
+class Stack {
+    constructor() {
         this.data = {};
         this.topIndex = -1;
     }
-    traverse(){
+    traverse() {
         let output = "";
-        for(let i=0; i<=this.topIndex; i++){
+        for (let i = 0; i <= this.topIndex; i++) {
             output += this.data[i] + " ";
         }
-         console.log(output);
+        console.log(output);
+    }
+    push(element) {
+        this.topIndex++;
+        this.data[this.topIndex] = element;
+    }
+    is_empty() {
+        return this.topIndex === -1;
+    }
+    peek() {
+        //correcting statement
+        if (this.is_empty()) return null; {
+            return this.data[this.topIndex];
+        }
+       
+
+    }
+    pop() {
+        if (this.is_empty()) {
+            console.log("Stack is empty");
+            return null;
+        }
 }
-push(element){
-    this.topIndex++;
-    this.data[this.topIndex] = element;
 }
-}
+
 let stack1 = new Stack();
 stack1.push(2);
-stack1.traverse();
 stack1.push(4);
-stack1.traverse();
 stack1.push(6);
+stack1.traverse()
+// console.log("is the stack rempty? " + stack1.is_empty());
+console.log("Top element is: " + stack1.peek());
+console.log("Top element is: " + stack1.pop() + "is popped, the new element is :" + stack1.peek());
 stack1.traverse();
+
+s
